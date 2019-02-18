f4a9ffdab4a59cc3c10c19ed7913c761cb579e74
Thanik Bhongbhibhat <email@thanik.me>
2019-02-18 10:48:23 -0800

    Add test: serial will not call next fn until current one resolves

--- lib/PromiseUtils.test.ts
+++ lib/PromiseUtils.test.ts
@@ -16,28 +16,47 @@ describe('PromiseUtils', () => {
       expect(result).toBe('init:foo:bar');
     });
 
+    it('does not execute the next function until the previous one resolves', async () => {
+      expect.assertions(5);
+
+      const p1 = jest.fn(async () => {
+        await delay(100);
+        expect(p2).not.toHaveBeenCalled();
+        expect(p3).not.toHaveBeenCalled();
+      });
+
+      const p2 = jest.fn(async () => {
+        await delay(100);
+        expect(p1).toHaveBeenCalledTimes(1);
+        expect(p2).toHaveBeenCalledTimes(1);
+        expect(p3).not.toHaveBeenCalled();
+      });
+
+      const p3 = jest.fn().mockReturnValue(delay(100));
+
+      await serial([p1, p2, p3], null);
+    });
+
     it('skips all functions after the function that rejectes the promise', async () => {
       expect.assertions(8);
 
-      const initial = 0;
-      const vs = [1, 5, 10, 50, 100];
-      const ps = vs.map((v: number, i: number) =>
+      const ps = [1, 5, 10, 50, 100].map((v: number, i: number) =>
         i === 2
           ? jest.fn().mockRejectedValue(Error('something went wrong'))
           : jest.fn((prev: number) => delay(100).then(() => prev + v))
       );
 
       try {
-        await serial(ps, initial);
+        await serial(ps, 0);
       } catch (e) {
-        expect(ps[0]).toHaveBeenCalled();
-        expect(ps[0]).toHaveBeenCalledWith(initial);
+        expect(ps[0]).toHaveBeenCalledTimes(1);
+        expect(ps[0]).toHaveBeenCalledWith(0);
 
-        expect(ps[1]).toHaveBeenCalled();
-        expect(ps[1]).toHaveBeenCalledWith(initial + vs[0]);
+        expect(ps[1]).toHaveBeenCalledTimes(1);
+        expect(ps[1]).toHaveBeenCalledWith(1);
 
-        expect(ps[2]).toHaveBeenCalled();
-        expect(ps[2]).toHaveBeenCalledWith(initial + vs[0] + vs[1]);
+        expect(ps[2]).toHaveBeenCalledTimes(1);
+        expect(ps[2]).toHaveBeenCalledWith(6);
 
         expect(ps[3]).not.toHaveBeenCalled();
         expect(ps[4]).not.toHaveBeenCalled();
